b717a18ec90d3b258e56a630b91a75b7c85afc33
goregore <aoushunpei.1107@gmail.com>
2025-08-02 18:04:11 +0900

    [update] day6

--- week2/day6/practice_solution.js
+++ week2/day6/practice_solution.js
@@ -3,17 +3,20 @@ console.log("JavaScript is loaded!");
 
 // 練習問題 2:
 // `id="main-title"`を持つ要素を`getElementById`で取得し、コンソールに出力してみましょう。
-const myElement = document.getElementById("main-title");
-console.log(myElement);
+const mainTitle = document.getElementById("main-title");
+console.log("--- 問題2の解答 ---");
+console.log(mainTitle);
 
 // 練習問題 3:
 // `class="paragraph"`を持つ最初の要素を`querySelector`で取得し、コンソールに出力してみましょう。
-const myClass = document.querySelector(".paragraph");
-console.log(myClass);
+const firstParagraph = document.querySelector(".paragraph");
+console.log("--- 問題3の解答 ---");
+console.log(firstParagraph);
 
 // 練習問題 4:
 // `class="paragraph"`を持つすべての要素を`querySelectorAll`で取得し、コンソールに出力してみましょう。
 // 取得したNodeListの`length`プロパティも表示してみましょう。
-const myAllClass = document.querySelectorAll(".paragraph");
-console.log(myAllClass);
-console.log(myAllClass.length);
+const allParagraphs = document.querySelectorAll(".paragraph");
+console.log("--- 問題4の解答 ---");
+console.log(allParagraphs);
+console.log("取得した要素の数:", allParagraphs.length);
